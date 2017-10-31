19432ec44d561f368f8e523a5c67df43f9d15e4f
Juan Perez <juanmarcperez@gmail.com>
2017-10-30 23:31:04 -0500

    indentations corrections

--- movieSearch.js
+++ movieSearch.js
@@ -64,18 +64,18 @@ function searchByGenre(genreName){
 function searchPeopleId(person){
 
   fetch('https://api.themoviedb.org/3/person/'+ person + '?api_key=6125b5ec93157ac1d7ec03c2a0ecbf16&language=en-US')
-  .then(function(response) {
-    if(response.status !== 200){
-      console.log('Looks like there was a problem. Status Code: ' + response.status);
-      return;
-  }
+    .then(function(response) {
+      if(response.status !== 200){
+        console.log('Looks like there was a problem. Status Code: ' + response.status);
+          return;
+        }
     //Examine the text in the response
     response.json().then(function(personInfo){
-      console.log(personInfo);
-      $('#result').empty();
-      $('#result').append('<p> Name: ' + personInfo.name + '</p>');
-      $('#result').append('<p> Biography: ' + personInfo.biography + '</p>');
-      $('#result').append('<p> Place of Birth: ' + personInfo.place_of_birth+ '</p>');
+        console.log(personInfo);
+          $('#result').empty();
+          $('#result').append('<p> Name: ' + personInfo.name + '</p>');
+          $('#result').append('<p> Biography: ' + personInfo.biography + '</p>');
+          $('#result').append('<p> Place of Birth: ' + personInfo.place_of_birth+ '</p>');
 
   });
   }
@@ -84,10 +84,10 @@ function searchPeopleId(person){
 }
 
 function doit_onkeypress(event){
-                if (event.keyCode == 13 || event.which == 13){
-                    searchMovies();
-                }
-            }
+      if (event.keyCode == 13 || event.which == 13){
+          searchMovies();
+    }
+}
 
 
 function searchMovies(){
@@ -96,22 +96,22 @@ function searchMovies(){
 
   switch(searchBy){
     case "Movie":
-    fetch('https://api.themoviedb.org/3/search/movie?api_key=6125b5ec93157ac1d7ec03c2a0ecbf16&query=' + inputData)
-    .then(function(response) {
-      if(response.status !== 200){
-        console.log('Looks like there was a problem. Status Code: ' + response.status);
-        return;
-        }
+      fetch('https://api.themoviedb.org/3/search/movie?api_key=6125b5ec93157ac1d7ec03c2a0ecbf16&query=' + inputData)
+        .then(function(response) {
+            if(response.status !== 200){
+                console.log('Looks like there was a problem. Status Code: ' + response.status);
+                  return;
+                }
       //Examine the text in the response
-      response.json().then(function(data){
-        console.log(data);
-        console.log(data.results[0].overview);
-        $('#result').empty();
-        $('#result').append('<img src =" '+ URL + data.results[0].poster_path+' " align ="left" </img>');
-        $('#result').append('<p> Movie Description: ' + data.results[0].overview+ '</p>');
-        $('#result').append('<p> Release Date: ' + data.results[0].release_date+ ' (yyyy,dd,mm)</p>');
-        $('#result').append('<p> Vote Average: ' + data.results[0].vote_average+ '</p>');
-        $('#result').append('<p> Original Language: ' + data.results[0].original_language+ '</p>');
+            response.json().then(function(data){
+                console.log(data);
+                console.log(data.results[0].overview);
+                  $('#result').empty();
+                  $('#result').append('<img src =" '+ URL + data.results[0].poster_path+' " align ="left" </img>');
+                  $('#result').append('<p> Movie Description: ' + data.results[0].overview+ '</p>');
+                  $('#result').append('<p> Release Date: ' + data.results[0].release_date+ ' (yyyy,dd,mm)</p>');
+                  $('#result').append('<p> Vote Average: ' + data.results[0].vote_average+ '</p>');
+                  $('#result').append('<p> Original Language: ' + data.results[0].original_language+ '</p>');
                 });
             }
         )// end fetch call
@@ -120,51 +120,51 @@ function searchMovies(){
         console.log('Fetch Error :-S', err);
 
         });
-      break;
+  break;
 
       case "TV Show":
       fetch('https://api.themoviedb.org/3/search/tv?api_key=6125b5ec93157ac1d7ec03c2a0ecbf16&language=en-US&query=' + inputData)
-      .then(function(response) {
-        if(response.status !== 200){
-          console.log('Looks like there was a problem. Status Code: ' + response.status);
-          return;
-      }
+        .then(function(response) {
+          if(response.status !== 200){
+            console.log('Looks like there was a problem. Status Code: ' + response.status);
+              return;
+            }
         //Examine the text in the response
-        response.json().then(function(data){
-          console.log(data);
-          $('#result').empty();
-          $('#result').append('<img src =" '+ URL + data.results[0].poster_path+' "> </img>');
-          $('#result').append('<p> Name: ' + data.results[0].name+ '</p>');
-          $('#result').append('<p> First Air Date: ' + data.results[0].first_air_date+ '</p>');
-          $('#result').append('<p> Overview: ' + data.results[0].overview+ '</p>');
-          $('#result').append('<p> Vote Average: ' + data.results[0].vote_average+ '</p>');
+          response.json().then(function(data){
+              console.log(data);
+                $('#result').empty();
+                $('#result').append('<img src =" '+ URL + data.results[0].poster_path+' "> </img>');
+                $('#result').append('<p> Name: ' + data.results[0].name+ '</p>');
+                $('#result').append('<p> First Air Date: ' + data.results[0].first_air_date+ '</p>');
+                $('#result').append('<p> Overview: ' + data.results[0].overview+ '</p>');
+                $('#result').append('<p> Vote Average: ' + data.results[0].vote_average+ '</p>');
                   });
               }
       )// end fetch call
 
       .catch(function(err){
-      console.log('Fetch Error :-S', err);
+        console.log('Fetch Error :-S', err);
 
       });
-      break;
+  break;
 
       case "Genre":
       searchId = searchByGenre (inputData);
       fetch('https://api.themoviedb.org/3/genre/'  + searchId +  '/movies?api_key=6125b5ec93157ac1d7ec03c2a0ecbf16&language=en-US&include_adult=true&sort_by=created_at.asc')
-      .then(function(response) {
-        if(response.status !== 200){
-          console.log('Looks like there was a problem. Status Code: ' + response.status);
-          return;
-      }
+        .then(function(response) {
+          if(response.status !== 200){
+            console.log('Looks like there was a problem. Status Code: ' + response.status);
+              return;
+            }
         //Examine the text in the response
-        response.json().then(function(data){
-          console.log(data);
-          $('#result').empty();
-          $('#result').append('<table>');
-          for (i = 0; i < data.results.length ; i++){
-          $('#result').append('<p> Name: ' + data.results[i].title+ '</p>');
-        }
-        $('#result').append('</table>');
+          response.json().then(function(data){
+            console.log(data);
+              $('#result').empty();
+              $('#result').append('<table>');
+                for (i = 0; i < data.results.length ; i++){
+                  $('#result').append('<p> Name: ' + data.results[i].title+ '</p>');
+                }
+              $('#result').append('</table>');
                 });
             }
       )// end then call
@@ -173,19 +173,18 @@ function searchMovies(){
       console.log('Fetch Error :-S', err);
 
       });
-      break;
+  break;
 
       case "People":
       fetch('https://api.themoviedb.org/3/search/person?api_key=6125b5ec93157ac1d7ec03c2a0ecbf16&language=en-US&query=' + inputData)
-      .then(function(response) {
-        if(response.status !== 200){
-          console.log('Looks like there was a problem. Status Code: ' + response.status);
-          return;
-      }
+        .then(function(response) {
+          if(response.status !== 200){
+              console.log('Looks like there was a problem. Status Code: ' + response.status);
+                return;
+              }
         //Examine the text in the response
         response.json().then(function(data){
           searchPeopleId(data.results[0].id);
-
                 });
             }
       )// end then call
@@ -199,23 +198,23 @@ function searchMovies(){
 }
 
 function tvToday(){
- fetch('https://api.themoviedb.org/3/tv/airing_today?api_key=6125b5ec93157ac1d7ec03c2a0ecbf16&language=en-US&page=1')
- .then(function(response) {
-   if(response.status !== 200){
-     console.log('Looks like there was a problem. Status Code: ' + response.status);
-     return;
-      }
+      fetch('https://api.themoviedb.org/3/tv/airing_today?api_key=6125b5ec93157ac1d7ec03c2a0ecbf16&language=en-US&page=1')
+        .then(function(response) {
+            if(response.status !== 200){
+              console.log('Looks like there was a problem. Status Code: ' + response.status);
+                return;
+              }
    //Examine the text in the response
-   response.json().then(function(data){
-     console.log(data);
-     for ( i=0; i< data.results.length;i++){
-     $('#todaysListings').append('<img src =" '+ URL + data.results[i].poster_path+' " </img>');
-     $('#todaysListings').append('<p> Name: ' + data.results[i].name+ '</p>');
-     $('#todaysListings').append('<p> First Air Date: ' + data.results[i].first_air_date+ '</p>');
-     $('#todaysListings').append('<p> Overview: ' + data.results[i].overview+ '</p>');
-     $('#todaysListings').append('<p> Vote Average ' + data.results[i].vote_average+ '</p>');
-     $('#todaysListings').append('<p> Original Name ' + data.results[i].original_name+ '</p>');
-      }
+        response.json().then(function(data){
+          console.log(data);
+            for ( i=0; i< data.results.length;i++){
+                $('#todaysListings').append('<img src =" '+ URL + data.results[i].poster_path+' " </img>');
+                $('#todaysListings').append('<p> Name: ' + data.results[i].name+ '</p>');
+                $('#todaysListings').append('<p> First Air Date: ' + data.results[i].first_air_date+ '</p>');
+                $('#todaysListings').append('<p> Overview: ' + data.results[i].overview+ '</p>');
+                $('#todaysListings').append('<p> Vote Average ' + data.results[i].vote_average+ '</p>');
+                $('#todaysListings').append('<p> Original Name ' + data.results[i].original_name+ '</p>');
+              }
               });
           }
       )
